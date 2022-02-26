83ea40d936d2ec1495f5ce4fee92903e8ac3f511
thienlehieu <68664959+thienlehieu@users.noreply.github.com>
2022-02-26 10:29:13 +0700

    Update key.ts

--- Backend/src/users/config/key.ts
+++ Backend/src/users/config/key.ts
@@ -1,3 +1,3 @@
 export default {
-    mongoURL: "mongodb+srv://thienle:300298@nodeexpressproject.g9ugb.mongodb.net/NestGrapqlReactProject?retryWrites=true&w=majority"
+    mongoURL: "PUT YOUR MONGO DB HERE"
 }
